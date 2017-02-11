05afa09efbe4a67d55d70636c8df405595d1b7a9
JackieLiu <liutong6053@163.com>
2017-02-11 21:27:58 +0800

    变更测试类名称。

--- src/org/jvmstudy/ch3/Ch3_6_1.java
+++ src/org/jvmstudy/ch3/Ch3_6_1.java
@@ -3,12 +3,14 @@ package org.jvmstudy.ch3;
 /**
  * 
  * @author jackieliu
- * VM参数： -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
  */
-public class MinorGc3_1 {
+public class Ch3_6_1 {
+	
 	private static final int _1MB = 1024*1024;
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		byte[] allocation1,allocation2,allocation3,allocation4;
+		
 		allocation1 = new byte[2*_1MB];
 		allocation2 = new byte[2*_1MB];
 		allocation3 = new byte[2*_1MB];
